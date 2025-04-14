fe79dd35e190130ee892cb5322dab6755aa967e6
Kauanzizz <160801849+Kauanzizz@users.noreply.github.com>
2025-04-14 19:03:11 -0300

    Update TesteMesa.java
    Adicionei um elemento a mais na lista (não gosto de números ímpares)

--- TrabalhoFinalRicardo/src/TesteMesa.java
+++ TrabalhoFinalRicardo/src/TesteMesa.java
@@ -1,8 +1,8 @@
 public class TesteMesa {
     public static void main(String[] args) {
-        ListaSimples listaSimples = new ListaSimples(5);
+        ListaSimples listaSimples = new ListaSimples(6);
 
-        Object elementos[] = {7, 3, 5, 2, 1};
+        Object elementos[] = {7, 3, 5, 2, 1, 10};
         listaSimples.inserirSequencia(elementos);
         listaSimples.ordenarCrescente();
     }
